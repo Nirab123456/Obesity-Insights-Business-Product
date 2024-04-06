59fa124377756e00fe10a0e4ef36915011448241
rifatul islam <rifatulislam210@gmail.com>
2024-04-06 17:25:22 -0400

    opened maui project

---
+++ obesity/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace obesity
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
